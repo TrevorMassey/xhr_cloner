ecb165cb5aa9e64910e686fe9532de529e616f90
smurf <TrevorMassey@users.noreply.github.com>
2019-08-24 11:57:08 -0700

    cleaned up last two content scripts

--- content_scripts/inject_monkey_patch.js
+++ content_scripts/inject_monkey_patch.js
@@ -1,14 +1,14 @@
 console.log("Running injection");
-var s = document.createElement('script');
+let s = document.createElement('script');
 
 s.src = chrome.runtime.getURL('content_scripts/monkey_patch.js');
 s.onload = function() {
     this.remove();
 };
-root = document.head || document.documentElement
+root = document.head || document.documentElement;
 if (root.firstChild) {
     console.log("Inserting before first child");
-    root.firstChild.insertBefore(s);
+    root.firstChild.insertBefore(s, root.firstChild);
 } else {
     console.log("No first child, appending");
     root.appendChild(s)
--- content_scripts/inject_script.js
+++ content_scripts/inject_script.js
@@ -1,4 +1,4 @@
-var s1 = document.createElement('script');
+let s1 = document.createElement('script');
 s1.src = chrome.runtime.getURL('content_scripts/monkey_patch.js');
 elem = (document.head || document.documentElement).appendChild(s1);
 console.log("Added Scripts");
